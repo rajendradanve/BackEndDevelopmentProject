d33a6bf502f831c6df2d3b3187c9e3fe22cf955c
Rajendra <rajendradanve@gmail.com>
2021-06-30 13:01:35 +0000

    contact form setup

--- run.py
+++ run.py
@@ -1,12 +1,17 @@
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
+if os.path.exists("eny.py"):
+    import env
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 @app.route("/about")
 def about():
@@ -14,6 +19,7 @@ def about():
     with open("data/company.json", "r") as json_data:
         data = json.load(json_data)
     return render_template("about.html", page_title="About", company= data)
+
 
 @app.route("/about/<member_name>")
 def about_member(member_name):
@@ -26,9 +32,15 @@ def about_member(member_name):
     
     return render_template("member.html", member = member)
 
-@app.route("/contact")
+
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        flash("Thanks {}, we have received your message!".format(
+            request.form.get("name")))
+
     return render_template("contact.html", page_title="Contact")
+
 
 @app.route("/careers")
 def careers():
